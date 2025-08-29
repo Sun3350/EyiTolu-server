e5381e8ff102005741e49c8e39176651bc15ecbe
Ayomide <isaacayomide2359@gmail.com>
2025-08-29 14:23:42 +0100

    update

--- api/Index.js
+++ api/Index.js
@@ -15,14 +15,10 @@ const PORT = process.env.PORT || 3001;
 
 // MongoDB Connection
 mongoose
-  .connect(
-    process.env.MONGODB_URI ||
-      "mongodb+srv://corevaulter:PmWt5keAR3rGIFRG@corevaulter.eaf2e4l.mongodb.net/",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
